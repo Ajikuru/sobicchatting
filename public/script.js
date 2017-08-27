9f341498e33965bf7250921defd0ea0208e9f8f0
Ajikuru <Ajikuru@gmail.com>
2017-08-28 00:44:14 +0100

    comit
    comit

--- public/script.js
+++ public/script.js
@@ -8,6 +8,10 @@ var msg  = $('textarea');
 var snd_id = $('#snd_id');
 var out_put = $('#output');
 var sat = $('#usr_stat');
+var Dept = $('#usr_dept');
+
+var chatter = $('#chatter');
+
 
 var reg_usr_name,reg_usr_id,reg_usr_pwd;
 var usr_name,usr_id;
@@ -47,10 +51,97 @@ var  cv_usr_rc = data.usr_rc;
 //var rec_id  = $('#rec_id');
 //alert(cv_msg);
 
+var per_t_rc = rec_id.val();
 //$('#show_conv').append("<li>" + cv_msg + "</li>");
 
 //	alert(cv_usr_snd);
 
+//var user_dept = {"DP01":"DP01","DP02":"DP02"}
+
+
+if(per_t_rc =="DP01" || per_t_rc =="DP02"){
+	//alert("It is there");
+}
+
+/*if(cv_usr_rc == rec_id.val()){
+
+	alert(rec_id.val());
+}*/
+//alert(cv_usr_rc + sat.val());
+
+if(cv_usr_rc =="DP01" || cv_usr_rc=="DP02"){
+
+//alert(cv_usr_snd + "joint" +rec_id.val());
+if(cv_usr_rc == rec_id.val()){
+
+	$('<div class="msg_a">'+ cv_msg +'</div>').insertBefore('.msg_push');
+	$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+}else if(sat.val()=="Online" && rec_id.val()!=cv_usr_rc){
+
+	//alert('yess');
+
+	var hii = $('#hint'+cv_usr_rc).attr('data-badge');
+
+	var ind = $('#hint'+cv_usr_rc).attr('data-ind','true');
+
+	//alert(cv_usr_rc);
+
+var snd_of_ind = cv_usr_rc;
+var rec_of_ind = usr_id.val();
+
+
+	//alert('Yess not available' + cv_usr_rc + usr_id.val());
+
+
+	if(hii==""){
+
+var new_un = 1 ;
+
+//$('#hint'+cv_usr_snd).text(new_un);
+
+}else{
+
+var new_un = parseInt(hii) + 1;
+
+}
+/*socket.emit('mark_msg',{snd_id: snd_of_ind,usr_rec:rec_of_ind},function(data){
+
+		if(data){
+
+				alert("yess");
+		}
+});*/
+
+	$('#hint'+cv_usr_rc).attr("data-badge",new_un);
+	//alert(cv_usr_rc + new_un);
+
+	var usr_ntf_b = 0;
+
+$('.badge1').each(function(i){
+		
+//		var usr_ntf = 0;
+		var value = $(this).attr('data-badge');
+		
+		var leng = value.length;
+	
+		//alert(value);				
+
+		if(value>0){
+			usr_ntf_b += 1; 
+		
+		}
+			
+	});
+
+/*if(usr_ntf_b==0){
+$('.badge2').attr('data-badge','');*/
+//}else{
+$('.badge2').attr('data-badge',usr_ntf_b);	
+
+}
+
+
+}else{
 
 if(cv_usr_snd == rec_id.val()){
 	//alert(sat.val());
@@ -61,10 +152,8 @@ if(cv_usr_snd == rec_id.val()){
 
 }else if(sat.val()=="Online" && rec_id.val()!=cv_usr_snd){
 
-
+	//alert('yess');
 var hii = $('#hint'+cv_usr_snd).attr('data-badge');
-
-
 
 var ind = $('#hint'+cv_usr_snd).attr('data-ind','true');
 
@@ -76,12 +165,9 @@ var new_un = 1 ;
 
 //$('#hint'+cv_usr_snd).text(new_un);
 
-
-
 }else{
 
 var new_un = parseInt(hii) + 1;
-
 
 }
 
@@ -91,7 +177,7 @@ socket.emit('mark_msg',{snd_id: cv_usr_snd,usr_rec:cv_usr_rc},function(data){
 
 		if(data){
 
-				alert("yess");
+				//alert("yess");
 		}
 });
 
@@ -135,11 +221,13 @@ $('.badge2').attr('data-badge',usr_ntf_b);
 
 		if(data){
 
-				alert("yess");
+				//alert("yess");
 		}
 	});
 	
 	}	
+
+			} // non_room
 });//out_put.html()
 
 
@@ -170,7 +258,8 @@ socket.emit('send-msg',{
 		user_id: snd_id.val(),
 		rec_id: rec_id.val(),
 		usr_name: user_name.val(),
-		msg: msg.val()
+		msg: msg.val(),
+		snd_nam: chatter.text()
 	},function(data){
 
 		if(data){
@@ -183,12 +272,14 @@ socket.emit('send-msg',{
 			$('<div class="msg_b">'+ msg +'</div>').insertBefore('.msg_push');
 			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
         	
+        	$(this).val('');
 		}
 	
 
 	});
 
 
+		
 }
 
 
@@ -198,12 +289,17 @@ socket.emit('send-msg',{
 
 $(document).on('click','#output > div',function(e){
 
+
+
 	e.preventDefault();
 
 	recid =  $(this).text();
 
-	var sub = recid.substr(0,4);
 	
+
+	var sub = recid.substr(0,4);
+
+
 	rec_id.val(sub);
 	
 	socket.emit('get_msg',{
@@ -222,8 +318,15 @@ $('#chat_user').text(sub +": " + chatto);
 
 if(ind_c=="true"){
 	
-	alert(rec_id.val());
+	//alert(rec_id.val());
 
+if(rec_id.val()== "DP01" || rec_id.val()=="DP02"){
+
+			$('#'+rec_id.val()).find('.badge1').attr('data-badge','');
+		//	var spn_chk = $(this).find('.badge1').attr('data-badge','');
+			$('#'+rec_id.val()).find('.badge1').attr('data-ind','false');
+	//alert("yS"+'false');
+}else{
 	socket.emit('mark_msg_rd',{user_id:usr_id.val(),rec_id: rec_id.val()},function(data){
 
 		if(data){
@@ -233,6 +336,11 @@ if(ind_c=="true"){
 			$('#'+rec_id.val()).find('.badge1').attr('data-ind','false');
 		}
 	});
+
+}
+
+
+
 
 	var rd_bag_cl = $('.badge2').attr('data-badge');
 
@@ -270,16 +378,38 @@ socket.on('snd_get_msg',function(data){
 	//alert(res.conversation);
 	//alert(data[0]['id']);	
 	//alert(data.conversation[0].id);	
-vis = "" +rec_id.val() + snd_id.val()+"";
-ver = "" + snd_id.val() + rec_id.val()+"";
+vis = ""+rec_id.val() + snd_id.val()+"";
+ver = ""+ snd_id.val() + rec_id.val()+"";
 
-	var chk = $.grep(res.conversation,function(element,index){
+	//alert(rec_id.val());
+
+	if(rec_id.val() == "DP01" || rec_id=="DP02"){
+
+			var chk = $.grep(res.conversation,function(element,index){
+
+				//element.conv_usr;
+				//return element.conv_usr == ver || element.conv_usr == vis;
+				return element.conv_usr.substring(4,8) == "DP01" || element.conv_usr.substring(4,8) == "DP02";
+
+
+		});
+
+	}else{
+
+		var chk = $.grep(res.conversation,function(element,index){
 
 				//element.conv_usr;
 				return element.conv_usr == ver || element.conv_usr == vis;
 
 
 		});
+	}
+
+
+	
+
+
+
 
 	$('#show_conv').html('');
 	
@@ -287,6 +417,8 @@ ver = "" + snd_id.val() + rec_id.val()+"";
 	$('.msg_b').remove();
 	$.each(chk,function(i){
 
+
+		//alert(chk[i].conv_usr);
 			//alert(chk[i].msg + chk[i].conv_usr);
 
 			if(chk[i].conv_usr.substr(0,4)==snd_id.val()){
@@ -312,7 +444,7 @@ $('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
 $(document).on('click','#login',function(){
 
 signed =  usr_id.val();
-	socket.emit('user_login',{usr_id:usr_id.val(),usr_pwd:usr_pwd.val()
+	socket.emit('user_login',{usr_id:usr_id.val(),usr_pwd:usr_pwd.val(),usr_dep:Dept.val()
 
 	},function(data){
 
@@ -382,14 +514,14 @@ socket.on('user_list',function(data){
 		
 	var cov_js = JSON.stringify(data.keys);
 
+
+	$('#chatter').html(data.log_nam);
 //	var conv = $.parseJSON(data.usr_unr_val);
 
 	//alert(data.usr_unr_key[0]);
 	//alert(data.usr_unr_val[0]);
 
 
-
-	
 
 	/*for(i=0; i<data.usr_unr_keys.length;i++){
 			alert(data.usr_unr_key[0]);
@@ -402,9 +534,25 @@ socket.on('user_list',function(data){
 
 		$('#list_all').html('');
 
+		//$('#output').append("<div class='user' id=></div>");
+
+		var lst_dpt = Dept.val();
+		var hint_dp = 'hint' + Dept.val();
+
+		//alert(lst_dpt.length);
+
+		if(lst_dpt==='DP01'){
+			var dept_nam = "Account";
+		}else if(lst_dpt==='DP02'){
+			var dept_nam = "Audit";
+		}
+
+		//	alert(dept_nam);
+		var app = $('#output').append("<div class='user' id='" + lst_dpt + "'>"+ Dept.val() + ": " + "<span style='font-weight:bold'>" + dept_nam + "</span>" + "  " +"<span class='badge1' data-ind = '' data-badge='' id=" + hint_dp + ">" + "</span>" +"</div>");
+
 	for(i=0; i<data.keys.length;i++){
 		
-		if(usr_id.val()!=data.keys[i]){
+		if(usr_id.val()!=data.keys[i] && data.keys[i]!="DP01" && data.keys[i]!="DP02" ){
 		//alert(data.val[i].details.length);	
 		
 		//alert(data.val[i].conn_stat); 
@@ -455,9 +603,6 @@ socket.on('user_list',function(data){
 	
 		$('#'+sub_ur).find('.badge1').attr('data-badge',d_t);
 	}
-
-
-	
 
 	/*if(data.val[i].usr_unred!=""){
 		 	var sta_ind = "true";
@@ -573,6 +718,7 @@ socket.on('disconn_usr',function(data){
 			if(msg!='')
 			$('<div class="msg_b">'+msg+'</div>').insertBefore('.msg_push');
 			$('.msg_body').scrollTop($('.msg_body')[0].scrollHeight);
+			$(this).val('');
         }
     });
 	
